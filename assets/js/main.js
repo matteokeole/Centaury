ff8004f9f136921e0be3827e2601670aa309abb5
Mattéo <matteoo34@icloud.com>
2021-10-01 20:13:08 +0200

    Blur

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-const landContainer = document.querySelector(".c");
+const landContainer = document.querySelector(".c"),
+landWrapper = document.querySelector(".d");
 window.addEventListener("scroll", function() {
 	let calc = this.scrollY / this.innerHeight;
+	// console.log(calc)
 	landContainer.style.backgroundSize = `${120 - (calc * 10)}%`;
-	landContainer.style.filter = `brightness(${100 - (calc * 100)}%)`
+	landContainer.style.filter = `brightness(${100 - (calc * 100)}%) blur(${calc * 20}px)`
 })
